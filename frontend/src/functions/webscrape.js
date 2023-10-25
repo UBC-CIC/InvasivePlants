9fdf25bd2a756efcd222c3be9f5ba25d14764e5d
Julia <juliayou604@gmail.com>
2023-10-25 16:04:28 -0700

    clean up code

--- frontend/src/functions/webscrape.js
+++ frontend/src/functions/webscrape.js
@@ -3,14 +3,12 @@ import axios from "axios";
 import { jsonrepair } from "jsonrepair";
 import { getDocument } from "pdfjs-dist";
 import "pdfjs-dist/build/pdf.worker.entry"; // Attach pdfJsworker to window
-import { getInvasiveSpeciesScientificNamesBC } from '../functions/pipeline';
 
 // List of website to links to invasive species website
 const BC_INVASIVE_URL = "https://bcinvasives.ca/take-action/identify/";
 const ON_INVASIVE_URL = "https://www.ontarioinvasiveplants.ca/invasive-plants/species/";
 const ON_INVASIVE_URL_AQUATIC_PLANTS = "https://www.invadingspecies.com/invaders/aquatic-plants/";
 const ON_INVASIVE_URL_TERRESTRIAL_PLANTS = "https://www.invadingspecies.com/invaders/terrestrial-plants/";
-const WIKIPEDIA_SEARCH_URL = "https://en.wikipedia.org/w/index.php?search=";
 
 /**
  *
@@ -370,130 +368,4 @@ const getListOfSpeciesFromONInvasive = async (output, url) => {
 
 };
 
-/**
- *
- * This function only webscrapes from wikipedia given the species name and collects the following:
- *  - species overview
- *  - species description
- *  - gallery of images
- *  - wiki url
- */
-const webscrapeWikipedia = async (scientificName, speciesScore) => {
-	try {
-		const searchUrl = `${WIKIPEDIA_SEARCH_URL}${encodeURIComponent(
-			scientificName
-		)}`;
-		const searchResponse = await axios.get(searchUrl);
-		const $ = cheerio.load(searchResponse.data);
-
-		const speciesInfo = {
-			scientificName: extractSpeciesName($),
-			speciesScore: speciesScore,
-			speciesOverview: extractSpeciesOverview($, scientificName),
-			speciesDescription: extractSpeciesDescription($),
-			speciesImages: extractSpeciesImages($),
-			isInvasive: await isInvasive(scientificName),
-			alternative_plants: await getAlternativePlants(await isInvasive(scientificName)),
-			searchUrl: searchUrl,
-		};
-
-		// console.log("info:", speciesInfo);
-		return speciesInfo;
-	} catch (error) {
-		console.error("Error while scraping wikipedia site:", error.message);
-	}
-};
-
-const getAlternativePlants = async (input) => {
-	let alternative_plants = []
-	if (input) {
-		// webscrape for the alternative plants
-
-	}
-	return alternative_plants
-}
-
-const fetchInvasiveSpeciesBC = async () => {
-	try {
-		const result = await getInvasiveSpeciesScientificNamesBC();
-		return result;
-		// console.log("BC invasive species: ", result);
-	} catch (error) {
-		console.error("Error fetching BC invasive species: ", error);
-	}
-};
-
-// is invasive
-// TODO: check location too
-const isInvasive = async (scientificName) => {
-	let invasiveListBC = await fetchInvasiveSpeciesBC();
-	return invasiveListBC.includes(scientificName);
-};
-
-// get species name
-const extractSpeciesName = ($) => {
-	return $("h1.firstHeading").text();
-};
-
-// get species overview (first paragraph of wiki article)
-const extractSpeciesOverview = ($, speciesName) => {
-	let speciesOverview = "";
-	$("div.mw-parser-output p").each(function () {
-		// break the loop after finding the first paragraph
-		if ($(this).text().startsWith(speciesName)) {
-			speciesOverview = $(this).text();
-			return false;
-		}
-	});
-	return cleanUpString(speciesOverview);
-};
-
-// get species description
-const extractSpeciesDescription = ($) => {
-	let speciesDescription = [];
-	let found = false;
-	$("h2 span.mw-headline#Description")
-		.parent()
-		.nextAll()
-		.each(function () {
-			// break the loop when the next h2 is found
-			if ($(this).is("h2")) {
-				found = true;
-				return false;
-			}
-			if (!found && $(this).is("p")) {
-				speciesDescription.push($(this).text());
-			}
-		});
-	return cleanUpString(speciesDescription);
-};
-
-// get species images
-const extractSpeciesImages = ($) => {
-	const speciesImages = [];
-	$("ul.gallery img").each((index, element) => {
-		let src = $(element).attr("src");
-		if (src && src.startsWith("//")) {
-			src = "https:" + src;
-		}
-		speciesImages.push(src);
-	});
-	return speciesImages;
-};
-
-// helper function to clean up data
-const cleanUpString = (input) => {
-	if (typeof input !== "string") {
-		input = input.toString();
-	}
-
-	// remove brackets []
-	input = input.replace(/\[[^\]]*\]/g, "");
-
-	// remove commas preceded by a line break and divide with a new line
-	input = input.replace(/(\r\n|[\r\n])\s*,/g, "$1\n");
-
-	return input.trim();
-};
-
-export { webscrapeBCInvasive, webscrapeONInvasive, webscrapeWikipedia };
+export { webscrapeBCInvasive, webscrapeONInvasive };
--- frontend/src/pages/pl@ntNet.js
+++ frontend/src/pages/pl@ntNet.js
@@ -1,32 +1,18 @@
 import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { saveAs } from 'file-saver';
-import { webscrapeBCInvasive, webscrapeONInvasive, webscrapeWikipedia } from '../functions/webscrape';
-import { webscrapeInvasiveSpecies, getInvasiveSpeciesScientificNamesBC, getInvasiveSpeciesScientificNamesON } from '../functions/pipeline';
-
-// const FormData = require('form-data');
+import { webscrapeWikipedia } from '../functions/webscrapeWiki';
+// import { webscrapeBCInvasive, webscrapeONInvasive } from '../functions/webscrape';
 
 function PlantNet() {
-    const fs = require('fs');
     const [selectedLanguage, setSelectedLanguage] = useState('en');
-    const [location, setLocation] = useState('BC');
+    const [location, setSelectedLocation] = useState('BC');
     const [selectedFile, setSelectedFile] = useState(null);
     const [modelResult, setModelResult] = useState(undefined);
     const [modelObjResult, setModelResultObj] = useState([]);
-    const [invasiveListBC, setInvasiveListBC] = useState([]);
-
-    const [speciesInfo, setSpeciesInfo] = useState([]);
-
     const [numImages, setNumImages] = useState(0);
-
-
+    const [isFileSaved, setIsFileSaved] = useState(false);
     const formRef = useRef(null);
-
-    // Handler for file input change
-    // const handleFileChange = (event) => {
-    //     console.log("event.target.files[0: ", typeof event.target.files[0], event.target.files[0]);
-    //     setSelectedFile(event.target.files[0]);
-    // };
 
     // Handle for submiting the form
     const handleSubmit = (event) => {
@@ -41,7 +27,7 @@ function PlantNet() {
             const image = formData.get('images' + i.toString());
             const organ = formData.get('organs' + i.toString());
 
-            if (image.name) {
+            if (image && image.name) {
                 formDataPlantNet.append('organs', organ);
                 formDataPlantNet.append('images', image);
 
@@ -74,66 +60,18 @@ function PlantNet() {
             });
     }
 
-    // Handler for form submission
-    // const handleUpload = () => {
-    //     if (selectedFile) {
-    //         let form = new FormData();
-
-    //         form.append('organs', 'auto');
-    //         form.append('images', selectedFile);
-
-    //         const project = 'all';
-    //         const url = 'https://my-api.plantnet.org/v2/identify/' + project + `?api-key=${process.env.REACT_APP_PLANTNET_API_KEY}`;
-    //         axios.post(url, form, {
-    //             headers: {
-    //                 // 'accept': 'application/json',
-    //                 'Content-Type': `multipart/form-data`
-    //             }
-    //         })
-    //             .then((response) => {
-    //                 console.log('File uploaded successfully:', response.data);
-    //                 // Handle success, e.g., show a success message to the user.
-
-    //                 setModelResult(JSON.stringify(response.data, null, 2));
-    //             })
-    //             .catch((error) => {
-    //                 console.error('Error uploading file:', error);
-    //                 // Handle error, e.g., show an error message to the user.
-    //             });
-    //     }
-    // };
-
-    // const handleUpload1 = () => {
-    //     // Define the API endpoint and request data
-    //     const apiUrl = `https://my-api.plantnet.org/v2/identify/all?api-key=${process.env.REACT_APP_PLANTNET_API_KEY}`;
-    //     const formData = new FormData();
-
-    //     // Add form data fields
-    //     formData.append('images', new Blob([selectedFile], { type: 'image/jpeg' }));
-    //     formData.append('organs', 'auto');
-
-    //     // Create headers
-    //     const headers = {
-    //         'accept': 'application/json',
-    //         'Content-Type': 'multipart/form-data'
-    //     };
-
-    //     // Make the Axios POST request
-    //     axios.post(apiUrl, formData, { headers })
-    //         .then(response => {
-    //             console.log('Response:', response.data);
-    //         })
-    //         .catch(error => {
-    //             console.error('Error:', error);
-    //         });
-    // };
-
     const handleLanguageSelection = (event) => {
-        console.log("event.target.value: ", event.target.value)
+        console.log("language: ", event.target.value)
         setSelectedLanguage(event.target.value);
     }
 
-    // const getSpeciesInfo = async (results) => {
+    const handleLocationSelection = (event) => {
+        console.log("location: ", event.target.value)
+        setSelectedLocation(event.target.value);
+    }
+
+    // gets all results
+    // const getSpeciesResultInfo = async (results) => {
     //     let speciesInfoArray = [];
     //     const promises = results.map(async (res) => {
     //         let scientificName = res.species.scientificNameWithoutAuthor;
@@ -146,137 +84,54 @@ function PlantNet() {
     //     return speciesInfoArray;
     // };
 
-    // only first three results
-    const getSpeciesInfo = async (results) => {
-        let speciesInfoArray = [];
-        const limit = Math.min(results.length, 3);
 
-        for (let i = 0; i < limit; i++) {
-            let res = results[i];
-            let scientificName = res.species.scientificNameWithoutAuthor;
-            let score = res.score;
-            let info = await webscrapeWikipedia(scientificName, score);
-            speciesInfoArray.push(info);
-            console.log(speciesInfoArray.length);
+    // top 3 results
+    const getSpeciesResultInfo = async (results) => {
+        let speciesInfoArray = [];
+        let count = 0; // Initialize the count variable
+
+        for (let i = 0; i < results.length; i++) {
+            if (count < 3) {
+                let res = results[i];
+                let commonName = res.species.commonName;
+                let scientificName = res.species.scientificNameWithoutAuthor;
+                let score = res.score;
+                let info = await webscrapeWikipedia(commonName, scientificName, score);
+                speciesInfoArray.push(info);
+                count++; // Increment the count
+                console.log(speciesInfoArray.length);
+            } else {
+                break; // Break the loop when the count reaches 3
+            }
         }
-        // console.log("all species: ", speciesInfoArray)
         return speciesInfoArray;
     };
 
-    useEffect(() => {
-        // Run BC Invasive webscraping script
-        // webscrapeBCInvasive();
-        // webscrapeONInvasive();
-        // webscrapeInvasiveSpecies();
 
-        // get species info
+    useEffect(() => {
         const fetchData = async () => {
             try {
-                const speciesInfoArray = await getSpeciesInfo(modelObjResult.results);
-                setSpeciesInfo(speciesInfoArray);
-                console.log("species info: ", speciesInfo)
+                if (!isFileSaved && modelObjResult && modelObjResult.results) {
+                    const speciesInfoArray = await getSpeciesResultInfo(modelObjResult.results);
+                    console.log("species info: ", speciesInfoArray)
 
-                if (speciesInfo.length === 3) {
-                    const data = JSON.stringify(speciesInfoArray, null, 2);
-                    const blob = new Blob([data], { type: 'application/json' });
+                    if (speciesInfoArray.length === Math.min(modelObjResult.results.length, 3)) {
+                        console.log("got here!!!")
+                        const data = JSON.stringify(speciesInfoArray, null, 2);
+                        const blob = new Blob([data], { type: 'application/json' });
 
-                    const fileName = 'speciesData.json';
+                        const fileName = 'speciesData.json';
 
-                    saveAs(blob, fileName);
-                    return;
+                        saveAs(blob, fileName);
+                        setIsFileSaved(true);
+                    }
                 }
             } catch (error) {
-                console.error("Error fetching or processing data:", error);
+                console.log("error getting species info: ", error);
             }
         };
         fetchData();
-    }, [modelObjResult.results, speciesInfo]);
-
-
-
-
-
-    // useEffect(() => {
-    //     // Run BC Invasive webscraping script
-    //     // webscrapeBCInvasive();
-    //     // webscrapeONInvasive();
-    //     // webscrapeInvasiveSpecies();
-
-    //     // webscrape wikipedia by getting the "scientificNameWithoutAuthor" for each result returned by API
-    //     const getSpeciesInfo = async () => {
-    //         if (modelObjResult && modelObjResult.results) {
-    //             // console.log("length: ", modelObjResult.results.length)
-    //             let speciesInfoArray = [];
-    //             for (let res of modelObjResult.results) {
-    //                 let scientificName = res.species.scientificNameWithoutAuthor;
-    //                 let score = res.score;
-    //                 // TODO: check based on user location
-    //                 // let isInvasive = invasiveListBC.includes(scientificName)
-    //                 // TODO: if invasive, include alternative plants
-    //                 let info = await webscrapeWikipedia(scientificName, score);
-    //                 speciesInfoArray.push(info);
-    //                 console.log(speciesInfoArray.length)
-    //             }
-
-    //             // Create a JSON file
-    //             if (speciesInfoArray.length === modelObjResult.results.length) {
-    //                 setSpeciesInfo(speciesInfoArray);
-    //                 console.log("species info: ", speciesInfo)
-
-    //                 const data = JSON.stringify(speciesInfoArray, null, 2);
-    //                 const blob = new Blob([data], { type: 'application/json' });
-
-    //                 const fileName = 'speciesData.json';
-
-    //                 // Check if the file exists and delete it if it does
-    //                 const existingFile = localStorage.getItem(fileName);
-    //                 if (existingFile) {
-    //                     localStorage.removeItem(fileName);
-    //                     console.log('Existing file deleted');
-    //                 }
-
-    //                 saveAs(blob, fileName);
-    //                 return;
-    //             }
-    //         }
-    //     };
-    //     getSpeciesInfo();
-    // }, [modelObjResult, invasiveListBC, speciesInfo]);
-
-
-
-
-    // const filePath = './data/speciesData.json';
-
-    // useEffect(() => {
-    //     const getSpeciesInfo = async () => {
-    //         if (modelObjResult && modelObjResult.results) {
-    //             let speciesInfoArray = [];
-    //             for (let res of modelObjResult.results) {
-    //                 let scientificName = res.species.scientificNameWithoutAuthor;
-    //                 let score = res.score;
-    //                 let info = await webscrapeWikipedia(scientificName, score);
-    //                 speciesInfoArray.push(info);
-    //             }
-    //             setSpeciesInfo(speciesInfoArray);
-    //         };
-    //     }
-
-    //     getSpeciesInfo();
-
-    //     // Create a JSON file
-    //     if (speciesInfo) {
-    //         const data = JSON.stringify(speciesInfo, null, 2);
-
-    //         fs.writeFile(filePath, data, (err) => {
-    //             if (err) {
-    //                 console.error('Error writing file', err);
-    //             } else {
-    //                 console.log('File written successfully');
-    //             }
-    //         });
-    //     }
-    // }, [modelObjResult, speciesInfo]);
+    }, [isFileSaved, modelObjResult]);
 
 
     return (
@@ -290,17 +145,24 @@ function PlantNet() {
                     <option value="fr">French</option>
                     <option value="de">German</option>
                 </select>
+
+                <select onChange={handleLocationSelection}>
+                    <option>-- Choose a Location --</option>
+                    <option>BC</option>
+                    <option>ON</option>
+                </select>
             </div>
 
             <div>
                 {numImages < 4 && <button onClick={() => { setNumImages(numImages + 1) }}>Add more organ</button>}
             </div>
+
             <form ref={formRef} onSubmit={handleSubmit}>
-                {numImages >= 0 &&
-                    <div>
-                        <input type="file" accept="image/*" name="images0" />
+                {[...Array(numImages).keys()].map((index) => (
+                    <div key={index}>
+                        <input type="file" accept="image/*" name={`images${index}`} />
 
-                        <select name="organs0">
+                        <select name={`organs${index}`}>
                             <option value="auto">-- Choose an organ --</option>
                             <option value="flower">Flower</option>
                             <option value="fruit">Fruit</option>
@@ -309,93 +171,28 @@ function PlantNet() {
                             <option value="auto">Auto</option>
                         </select>
                     </div>
-                }
-
-                {numImages >= 1 &&
-                    <div>
-                        <input type="file" accept="image/*" name="images1" />
-
-                        <select name="organs1">
-                            <option value="auto">-- Choose an organ --</option>
-                            <option value="flower">Flower</option>
-                            <option value="fruit">Fruit</option>
-                            <option value="leaf">Leaf</option>
-                            <option value="bark">Bark</option>
-                            <option value="auto">Auto</option>
-                        </select>
-                    </div>
-                }
-
-                {numImages >= 2 &&
-                    <div>
-                        <input type="file" accept="image/*" name="images2" />
-
-                        <select name="organs2">
-                            <option value="auto">-- Choose an organ --</option>
-                            <option value="flower">Flower</option>
-                            <option value="fruit">Fruit</option>
-                            <option value="leaf">Leaf</option>
-                            <option value="bark">Bark</option>
-                            <option value="auto">Auto</option>
-                        </select>
-                    </div>
-                }
-
-                {numImages >= 3 &&
-                    <div>
-                        <input type="file" accept="image/*" name="images3" />
-
-                        <select name="organs3">
-                            <option value="auto">-- Choose an organ --</option>
-                            <option value="flower">Flower</option>
-                            <option value="fruit">Fruit</option>
-                            <option value="leaf">Leaf</option>
-                            <option value="bark">Bark</option>
-                            <option value="auto">Auto</option>
-                        </select>
-                    </div>
-                }
-
-                {numImages >= 4 &&
-                    <div>
-                        <input type="file" accept="image/*" name="images4" />
-
-                        <select name="organs4">
-                            <option value="auto">-- Choose an organ --</option>
-                            <option value="flower">Flower</option>
-                            <option value="fruit">Fruit</option>
-                            <option value="leaf">Leaf</option>
-                            <option value="bark">Bark</option>
-                            <option value="auto">Auto</option>
-                        </select>
-                    </div>
-                }
-
+                ))}
                 <button type="submit">Submit</button>
-                {/* <button onClick={handleUpload}>Upload</button> */}
             </form>
-
-
 
             <div style={{
                 display: 'flex',
                 flexDirection: 'column',
                 alignItems: 'center'
             }}>
-                {selectedFile && <img src={URL.createObjectURL(selectedFile)} style={{
+                {selectedFile && <img src={URL.createObjectURL(selectedFile)} alt="" style={{
                     maxWidth: '500px',
                     padding: '2%'
                 }} />}
 
-                {modelResult && <React.Fragment>
+                {modelResult && (
                     <pre style={{ textAlign: 'left' }}>{modelResult}</pre>
-                </React.Fragment>}
+                )}
             </div>
         </React.Fragment>
     );
-}
+};
 
-// module.exports = typeof window.self == 'object' ? window.self.FormData : window.FormData;
 const FormDataExport = typeof window.self === 'object' ? window.self.FormData : window.FormData;
 
 export { FormDataExport, PlantNet };
