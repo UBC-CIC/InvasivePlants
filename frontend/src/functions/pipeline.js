a3f339cfa16520ccd24fb83e93d8ffd118ac7a72
vsaosuo <visal.saosuo@gmail.com>
2023-10-26 13:34:13 -0700

    Created function to flag species that need info on

--- frontend/src/functions/pipeline.js
+++ frontend/src/functions/pipeline.js
@@ -1,4 +1,5 @@
 import { webscrapeBCInvasive, webscrapeONInvasive } from "./webscrape";
+import axios from 'axios';
 
 const webscrapeInvasiveSpecies = async () => {
     const region = [];
@@ -23,6 +24,57 @@ const webscrapeInvasiveSpecies = async () => {
 
     console.log("Region: ", region);
     return region;
-} 
+}
 
-export {webscrapeInvasiveSpecies};
+const flagedSpeciesBasicInfo = (speciesList) => {
+    const speciesFlagged = [];
+    speciesList.map((species) => {
+        if(!species.scientificName || species.scientificName === "" || !species.commonName || species.commonName === "")
+            speciesFlagged.push(species);
+    });
+
+    return speciesFlagged
+};
+
+const flagedSpeciesToPlanetAPI = async (speciesList) => {
+    const speciesFlagged = [];
+
+    const speciesXregion = await webscrapeInvasiveSpecies();
+
+    // Make request to Pl@ntNet API for list of species
+    const url = "https://my-api.plantnet.org/v2/species" + `?api-key=2b1006HUEA8IFmECZopWtUh73e`;                           /////////// HARD CODED!!!
+    await axios.get(url)
+        .then((response) => {
+            speciesXregion.map((region) => {
+                region.invasiveSpeciesList.map((species) => {
+                    // Split string into multiple strings
+                    const scienceN = species.scientificName ? species.scientificName.split('&') : [];
+
+                    if(species.scientificName){
+                        scienceN.map((sci) => {
+                            // Check based on scientific name and also common name
+                            if(!response.data.find(s => s.scientificNameWithoutAuthor.toLowerCase().includes(sci.toLowerCase()))
+                                || response.data.find(s => s.commonNames.some(str => str.toLowerCase().includes(sci.toLowerCase()))))
+                                speciesFlagged.push(species);
+                        });
+                    } else {
+                        speciesFlagged.push(species);
+                    }
+                }
+                );
+            });
+
+            // Handle success, e.g., show a success message to the user.
+            console.log('Species list: ', response.data);
+        })
+        .catch((error) => {
+            console.error('Error requesting species list:', error);
+            // Handle error, e.g., show an error message to the user.
+        });
+
+    console.log("speciesFlagged:", speciesFlagged);
+    return speciesFlagged
+}
+
+
+export {webscrapeInvasiveSpecies, flagedSpeciesToPlanetAPI};
