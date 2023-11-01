4f428c4a34ea6fc613fdfa257f02567a4f8c4b1a
Yuheng <yuhengzhang1016@gmail.com>
2023-11-01 10:02:36 -0700

    initial implentation of plant info category page

---
+++ flutter_app/lib/plant_info_from_category_page.dart
@@ -0,0 +1,123 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+
+class PlantInfoFromCategoryPage extends StatefulWidget {
+  final String plantName;
+
+  const PlantInfoFromCategoryPage({super.key, required this.plantName});
+
+  @override
+  _PlantInfoFromCategoryPageState createState() =>
+      _PlantInfoFromCategoryPageState();
+}
+
+class _PlantInfoFromCategoryPageState extends State<PlantInfoFromCategoryPage> {
+  bool isBookmarked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.plantName), // Plant name as app bar title
+        actions: [
+          IconButton(
+            icon: isBookmarked
+                ? const Icon(Icons.bookmark)
+                : const Icon(Icons.bookmark_border),
+            onPressed: () {
+              setState(() {
+                isBookmarked = !isBookmarked;
+              });
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  color: Colors.grey,
+                  height: MediaQuery.of(context).size.height / 3,
+                  child: const Center(
+                    child: Text('Left Rectangle'), // Left-side content
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.grey,
+                  height: MediaQuery.of(context).size.height / 3,
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Container(
+                          color: Colors.grey,
+                          height: MediaQuery.of(context).size.height / 6,
+                          child: const Center(
+                            child:
+                                Text('Top Right Square'), // Top-right content
+                          ),
+                        ),
+                        Container(
+                          color: Colors.grey,
+                          height: MediaQuery.of(context).size.height / 6,
+                          child: const Center(
+                            child: Text(
+                                'Bottom Right Square'), // Bottom-right content
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const Spacer(),
+          DefaultTabController(
+            length: 3, // Number of tabs
+            child: Column(
+              children: <Widget>[
+                const TabBar(
+                  tabs: [
+                    Tab(text: 'About'),
+                    Tab(text: 'Characteristics'),
+                    Tab(text: 'Control'),
+                  ],
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 3,
+                  child: TabBarView(
+                    children: [
+                      Center(child: Text('About ${widget.plantName}')),
+                      Center(
+                          child:
+                              Text('Characteristics of ${widget.plantName}')),
+                      Center(
+                          child:
+                              Text('Control procedure of ${widget.plantName}')),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: const BottomAppBar(
+        color: Colors.grey,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(Icons.home_outlined),
+            Icon(Icons.camera_alt_outlined),
+            Icon(Icons.bookmark_border),
+          ],
+        ),
+      ),
+    );
+  }
+}
