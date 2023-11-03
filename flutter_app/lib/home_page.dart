724f20ecc48b180b0c5d196e8b5179950e672a22
Yuheng <yuhengzhang1016@gmail.com>
2023-11-03 14:45:45 -0700

    home page complete for now

--- flutter_app/lib/home_page.dart
+++ flutter_app/lib/home_page.dart
@@ -1,7 +1,6 @@
-// ignore_for_file: library_private_types_in_public_api
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/settings_page.dart';
 import 'category_info_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,101 +13,155 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String selectedLocation = 'British Columbia';
   bool isBCSelected = true;
+  String searchText = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
-        leading: const CircleAvatar(
-          backgroundColor: Colors.black,
+        elevation: 0,
+        leading: Padding(
+          padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SettingsPage(),
+                ),
+              );
+            },
+            child: Image.asset(
+              'assets/images/profile.png',
+              width: 24,
+              height: 24,
+            ),
+          ),
         ),
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             const Icon(
               Icons.location_on,
               color: Colors.black,
             ),
             const SizedBox(width: 5),
-            DropdownButton<String>(
-              value: selectedLocation,
-              items:
-                  <String>['British Columbia', 'Ontario'].map((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedLocation = newValue ?? selectedLocation;
-                  isBCSelected = newValue == 'British Columbia';
-                });
-              },
-            ),
-          ],
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            const CupertinoSearchTextField(),
-            const SizedBox(height: 10),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: GridView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 1.2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                ),
-                itemCount: 12,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      // Navigate to CategoryInfoPage with the selected category title
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => CategoryInfoPage(
-                            categoryTitle:
-                                '${isBCSelected ? 'BC' : 'ONTARIO'} SPECIES ${index + 1}',
-                          ),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      color: isBCSelected ? Colors.blue : Colors.green,
-                      child: Center(
-                        child: Text(
-                          '${isBCSelected ? 'BC' : 'ONTARIO'} SPECIES ${index + 1}',
-                          style: const TextStyle(
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
-                    ),
+            DropdownButtonHideUnderline(
+              child: DropdownButton<String>(
+                value: selectedLocation,
+                items:
+                    <String>['British Columbia', 'Ontario'].map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
                   );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedLocation = newValue ?? selectedLocation;
+                    isBCSelected = newValue == 'British Columbia';
+                  });
                 },
               ),
             ),
-            const SizedBox(height: 10),
           ],
         ),
       ),
-      bottomNavigationBar: const BottomAppBar(
-        color: Colors.grey,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Icon(Icons.home_outlined),
-            Icon(Icons.camera_alt_outlined),
-            Icon(Icons.bookmark_border),
-          ],
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(15, 0, 15, 15),
+            child: CupertinoSearchTextField(
+              placeholder: 'Search',
+              onChanged: (value) {
+                setState(() {
+                  searchText = value;
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: GridView.count(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    crossAxisCount: 2,
+                    childAspectRatio: 1.2,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    children: _buildMatchingItems(),
+                  ),
+                ),
+                const SizedBox(height: 10),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_rounded, size: 40),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera_alt_outlined, size: 40),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark_border, size: 40),
+            label: '',
+          ),
+        ],
+      ),
+    );
+  }
+
+  List<Widget> _buildMatchingItems() {
+    List<Widget> matchingItems = [];
+
+    for (int index = 0; index < 12; index++) {
+      final speciesName =
+          '${isBCSelected ? 'BC' : 'ONTARIO'} SPECIES ${index + 1}';
+      if (searchText.isEmpty ||
+          speciesName.toLowerCase().contains(searchText.toLowerCase())) {
+        matchingItems.add(_buildGridItem(speciesName));
+      }
+    }
+
+    return matchingItems;
+  }
+
+  Widget _buildGridItem(String speciesName) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => CategoryInfoPage(
+              categoryTitle: speciesName,
+            ),
+          ),
+        );
+      },
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: Container(
+          color: isBCSelected ? Colors.blue : Colors.green,
+          child: Center(
+            child: Text(
+              speciesName,
+              style: const TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          ),
         ),
       ),
     );
