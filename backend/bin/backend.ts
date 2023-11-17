5d1a1e788b27cf9e4ed8dfd6bd06002ef4467212
vsaosuo <visal.saosuo@gmail.com>
2023-11-17 11:56:56 -0800

    instantiate new stack

--- backend/bin/backend.ts
+++ backend/bin/backend.ts
@@ -4,6 +4,7 @@ import { APIStack } from '../lib/api-stack';
 import { VpcStack } from '../lib/vpc-stack';
 import {DBStack} from '../lib/database-stack';
 import {DBFlowStack} from '../lib/dbFlow-stack';
+import { FunctionalityStack } from '../lib/functionality-stack';
 
 // Create application
 const app = new cdk.App();
@@ -19,3 +20,11 @@ const dbFlowStack = new DBFlowStack(app, 'DBFlowStack', vpcStack, dbStack);
 
 // Create an instance of an API stack
 const api = new APIStack(app, 'APIStack', vpcStack, dbStack);
+
+// Create an instance of functionality stack
+const functionality = new FunctionalityStack(app, 'FunctionalityStack');
+
+
+// aws cognito-idp sign-up --client-id 7factsbgi05qv66mgbk9cca827 --username visal.social@gmail.com --password 1234Abcd^ --region ca-central-1
+// aws cognito-idp admin-confirm-sign-up --user-pool-id ca-central-1_n7qsRCn5x --username visal.social@gmail.com --region ca-central-1
+// aws cognito-idp initiate-auth --auth-flow USER_PASSWORD_AUTH --client-id 7factsbgi05qv66mgbk9cca827 --auth-parameters USERNAME=visal.social@gmail.com,PASSWORD=1234Abcd^ --region ca-central-1
