39d389fd0185217140aa27ce99198263db7c027e
vsaosuo <visal.saosuo@gmail.com>
2023-11-20 11:06:39 -0800

    instantiate new stacks like host

--- backend/bin/backend.ts
+++ backend/bin/backend.ts
@@ -5,7 +5,8 @@ import { VpcStack } from '../lib/vpc-stack';
 import {DBStack} from '../lib/database-stack';
 import {DBFlowStack} from '../lib/dbFlow-stack';
 import { FunctionalityStack } from '../lib/functionality-stack';
-
+import { EcrStack } from '../lib/ecr-stack';
+import { HostStack } from '../lib/host-stack';
 // Create application
 const app = new cdk.App();
 
@@ -24,6 +25,8 @@ const api = new APIStack(app, 'APIStack', vpcStack, dbStack);
 // Create an instance of functionality stack
 const functionality = new FunctionalityStack(app, 'FunctionalityStack');
 
+// Create an instance of Host stack
+const hostStack = new HostStack(app, 'hostStack', vpcStack, functionality);
 
 // aws cognito-idp sign-up --client-id 7factsbgi05qv66mgbk9cca827 --username visal.social@gmail.com --password 1234Abcd^ --region ca-central-1
 // aws cognito-idp admin-confirm-sign-up --user-pool-id ca-central-1_n7qsRCn5x --username visal.social@gmail.com --region ca-central-1
