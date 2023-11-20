54de03e088769d146e783bb53ef3e541314a1cb0
vsaosuo <visal.saosuo@gmail.com>
2023-11-20 11:07:48 -0800

    created a host stack

--- backend/lib/functionality-stack.ts
+++ backend/lib/functionality-stack.ts
@@ -6,7 +6,10 @@ import * as cognito from 'aws-cdk-lib/aws-cognito';
 import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 
 export class FunctionalityStack extends cdk.Stack {
-    public readonly secretManager: secretsmanager.Secret;
+    public readonly secret: secretsmanager.ISecret;
+    public readonly userpoolId: string;
+    public readonly appClientId: string;
+    public readonly region: string;
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
@@ -58,18 +61,18 @@ export class FunctionalityStack extends cdk.Stack {
          * Store secrets to Secret Manager
          * User pool id, client id, and region the user pool deployed
          */
-        const userpoolId = userpool.userPoolId;
-        const appClientId = appClient.userPoolClientId;
-        const region = this.region;
+        this.userpoolId = userpool.userPoolId;
+        this.appClientId = appClient.userPoolClientId;
+        this.region = this.region;
 
         const secretsName = "Invasive_Plants_Cognito_Secrets"
-        this.secretManager = new secretsmanager.Secret(this, secretsName, {
+        this.secret = new secretsmanager.Secret(this, secretsName, {
             secretName: secretsName,
             description: "Cognito Secrets for authentication",
             secretObjectValue: {
-                REACT_APP_USERPOOL_ID: cdk.SecretValue.unsafePlainText(userpoolId),
-                REACT_APP_USERPOOL_WEB_CLIENT_ID: cdk.SecretValue.unsafePlainText(appClientId),
-                REACT_APP_REGION: cdk.SecretValue.unsafePlainText(region)
+                REACT_APP_USERPOOL_ID: cdk.SecretValue.unsafePlainText(this.userpoolId),
+                REACT_APP_USERPOOL_WEB_CLIENT_ID: cdk.SecretValue.unsafePlainText(this.appClientId),
+                REACT_APP_REGION: cdk.SecretValue.unsafePlainText(this.region)
             },
             removalPolicy: cdk.RemovalPolicy.DESTROY
         });
