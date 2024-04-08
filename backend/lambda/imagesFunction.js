9678a15b2e4f4281711dee0dfae497c2de9a3613
Julia <juliayou604@gmail.com>
2024-04-08 11:39:38 -0700

    add X-Amz-Security-Token to CORS header

--- backend/lambda/imagesFunction.js
+++ backend/lambda/imagesFunction.js
@@ -16,7 +16,7 @@ exports.handler = async (event) => {
 	const response = {
 		statusCode: 200,
 		headers: {
-            "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key",
+            "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "*"
         },
